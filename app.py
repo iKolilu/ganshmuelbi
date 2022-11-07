34036112750a03d512bf8f8570b2ffe8874d82aa
Timothy Owusu <codesnweed@gmail.com>
2022-11-07 14:05:23 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+
+app = Flask('__main__',template_folder='templates')
+
+@app.route("/")
+def home():
+    return "Welcome to the Gan Shmuel"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
