17b2bb32b785ced10e5cd77debab28aece9b3282
Emmanuel Cromwell <emmanuelcromwell@ymail.com>
2022-11-07 15:49:16 +0000

    Initialized API endpoint definitions.

--- Weight/app.py
+++ Weight/app.py
@@ -1,11 +1,42 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 
 
-app = Flask('__main__',template_folder='templates')
+app = Flask('__main__', template_folder='templates')
+
 
 @app.route("/")
 def home():
     return "Welcome to the Gan Shmuel Weight"
+
+
+@app.route("/weigth", methods=["GET", "POST"])
+def weight():
+    return "Not implemented"
+
+
+@app.route("/batch-weight", methods="POST")
+def batch_weight():
+    return "Not implemented"
+
+
+@app.route("/unknown", methods="GET")
+def unknown():
+    return "Not implemented"
+
+
+@app.route("/item/<id>", methods="GET")
+def item(id):
+    return "Not implemented"
+
+
+@app.route("/session/<id>", methods="GET")
+def session(id):
+    return "Not implemented"
+
+
+@app.route("/health", methods="GET")
+def health():
+    return "Not implemented"
 
 
 if __name__ == '__main__':
