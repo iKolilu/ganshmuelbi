4764eb483f9560005d86b476a752f7f23eb9550d
Timothy Owusu <codesnweed@gmail.com>
2022-11-07 22:21:39 +0000

    Correction of Error on methods

--- Weight/app.py
+++ Weight/app.py
@@ -14,30 +14,30 @@ def weight():
     return "Not implemented"
 
 
-@app.route("/batch-weight", methods="POST")
+@app.route("/batch-weight", methods=["POST"])
 def batch_weight():
     return "Not implemented"
 
 
-@app.route("/unknown", methods="GET")
+@app.route("/unknown", methods=["GET"])
 def unknown():
     return "Not implemented"
 
 
-@app.route("/item/<id>", methods="GET")
+@app.route("/item/<id>", methods=["GET"])
 def item(id):
     return "Not implemented"
 
 
-@app.route("/session/<id>", methods="GET")
+@app.route("/session/<id>", methods=["GET"])
 def session(id):
     return "Not implemented"
 
 
-@app.route("/health", methods="GET")
+@app.route("/health", methods=["GET"])
 def health():
     return "Not implemented"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True, host='0.0.0.0', port=7007)
