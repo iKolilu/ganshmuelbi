bb595d1d2e58414c858427ec0919c7d9d33581b2
Emmanuel <manuelxtrem@users.noreply.github.com>
2022-11-09 15:58:06 +0000

    create endpoints for day 2

--- Billing/Billing.py
+++ Billing/Billing.py
@@ -1,7 +1,12 @@
+
+
 import db
 from flask import Flask, jsonify, make_response, render_template, request
+from library import get_date_range, make_error, make_success
+from remote import get_item_from_weights
 
 app = Flask(__name__)
+database = db.connect()
 
 @app.route("/")
 def home():
@@ -10,14 +15,14 @@ def home():
 @app.route("/provider", methods=["POST"])
 def provider_create():
   new_name = request.json.get('name')
-  provider_id = db.create_provider(new_name)
-  return jsonify({"id": provider_id})
+  provider_id = db.create_provider(database, new_name)
+  return make_success({"id": provider_id})
 
 @app.route("/provider/<id>", methods=["PUT"])
 def provider_update(id):
   new_name = request.json.get('name')
-  db.update_provider(id, new_name)
-  return jsonify({"id": id, "name": new_name})
+  db.update_provider(database, id, new_name)
+  return make_success({"id": id, "name": new_name})
 
 @app.route("/rates", methods=["GET"])
 def rates_get():
@@ -29,15 +34,38 @@ def rates_create():
     
 @app.route("/truck", methods=["POST"])
 def truck_create():
-  return "Not implemented"
+  number_plate = request.json.get('number_plate')
+  provider_id = request.json.get('provider_id')
+  truck = db.create_truck(database, number_plate, provider_id)
+  return make_success({"id": truck})
 
 @app.route("/truck/<id>", methods=["PUT"])
 def truck_update(id):
-  return "Not implemented"
+  provider_id = request.json.get('provider_id')
+  
+  truck = db.update_truck(database, id, provider_id)
+  
+  return make_success({"id": truck, "provider_id": provider_id})
 
 @app.route("/truck/<id>", methods=["GET"])
 def truck_get(id):
-  return "Not implemented"
+  _from = request.args.get('from')
+  _to = request.args.get('to')
+  
+  dates = get_date_range(_from, _to)
+
+  # check db first
+  truck = db.get_truck(database, id)
+
+  if not truck:
+    return make_error(404, 'not found')
+
+  truck_data = get_item_from_weights(id, dates)
+
+  if not truck_data:
+    return make_error(404, 'not found')
+
+  return make_success(truck_data)
 
 @app.route("/bill/<id>", methods=["GET"])
 def bill_get(id):
@@ -45,10 +73,10 @@ def bill_get(id):
 
 @app.route("/health", methods=["GET"])
 def health():
-  if db.connect():
+  if db.connect() != None:
     return make_response("OK", 200)
     
   return make_response("Failure", 500)
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True, host='0.0.0.0')
--- Billing/db.py
+++ Billing/db.py
@@ -1,36 +1,69 @@
-import uuid
-
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="billdb"
-)
-
-mycursor = mydb.cursor()
 
 def connect():
-  # TODO: implement this 
-  return True
+  connection = None
+  try:
+    connection = mysql.connector.connect(
+      host="mysql",
+      port=3306,
+      user="root",
+      password="password",
+      database="billdb"
+    )
+    print("MySQL Database connection successful")
+  except mysql.Error as err:
+    print(f"Error: '{err}'")
 
+  return connection
 
-def get_provider(id):
+def get_provider(connection, id):
   return "Not implemented"
 
-def create_provider(name):
+def create_provider(connection, name):
+  cursor = connection.cursor()
+
   sql = "INSERT INTO Provider (id, name) VALUES (%s, %s);"
   val = (None, name)
-  mycursor.execute(sql, val)
-  mydb.commit()
+  cursor.execute(sql, val)
+  connection.commit()
 
-  return mycursor.lastrowid
+  return cursor.lastrowid
 
-def update_provider(id, name):
+def update_provider(connection, id, name):
+  cursor = connection.cursor()
+  
   sql = "UPDATE Provider SET name = %s WHERE id = %s;"
   val = (name, id)
-  mycursor.execute(sql, val)
-  mydb.commit()
+  cursor.execute(sql, val)
+  connection.commit()
 
-  return mycursor.lastrowid
+  return cursor.lastrowid
+
+def get_truck(connection, number_plate):
+  cursor = connection.cursor()
+
+  sql = "SELECT * FROM Trucks WHERE id = %s;"
+  val = (number_plate,)
+  cursor.execute(sql, val)
+  return cursor.fetchone()
+
+def create_truck(connection, number_plate, provider_id):
+  cursor = connection.cursor()
+
+  sql = "INSERT INTO Trucks (id, provider_id) VALUES (%s, %s);"
+  val = (number_plate, provider_id)
+  cursor.execute(sql, val)
+  connection.commit()
+
+  return number_plate
+
+def update_truck(connection, number_plate, provider_id):
+  cursor = connection.cursor()
+  
+  sql = "UPDATE Trucks SET provider_id = %s WHERE id = %s;"
+  val = (provider_id, number_plate)
+  cursor.execute(sql, val)
+  connection.commit()
+  
+  return number_plate
