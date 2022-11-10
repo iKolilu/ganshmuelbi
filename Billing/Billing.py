cdc252b099c3e4f70a81c8860d9f8208ca114acd
RJ-0605 <64560428+RJ-0605@users.noreply.github.com>
2022-11-10 13:09:19 +0000

    Billing day2 - GET & POST /rates (#5)
    * the post rates implementation is done
    
    * requirements.txts file and .gitignore done
    
    * corrected the requirements.txt file
    
    * fixed Post for rates endpoint
    
    * changes to database form xlsx is done and works
    
    * endpoint to return excel is done
    
    * GET path for rate to return xlsx
    
    * GET/rates  edit made to file path for rate to return xlsx
    
    * error handling with try except on POST/rates api
    
    * remove post rates file upload
    
    * cropping of unnecessary apis and addition of testing kit

--- Billing/Billing.py
+++ Billing/Billing.py
@@ -1,16 +1,26 @@
 
 
 import db
-from flask import Flask, jsonify, make_response, render_template, request
+from flask import Flask, jsonify, make_response, render_template, request, send_file
 from library import get_date_range, make_error, make_success
 from remote import get_item_from_weights
 
+from werkzeug.utils import secure_filename
+
+import os
+import openpyxl
+
+import csv
 app = Flask(__name__)
 database = db.connect()
 
 @app.route("/")
 def home():
-  return "Welcome to the Gan Shmuel Billing"
+  return render_template("index.html")
+
+@app.route("/test_route")
+def test_route():
+  return jsonify({'message': 'Hello, world!'})
 
 @app.route("/provider", methods=["POST"])
 def provider_create():
@@ -24,14 +34,57 @@ def provider_update(id):
   db.update_provider(database, id, new_name)
   return make_success({"id": id, "name": new_name})
 
+
+
 @app.route("/rates", methods=["GET"])
 def rates_get():
-  return "Not implemented"
+
+  
+  dir_name = os.path.join("in", 'rates.xlsx')
+
+  return send_file(dir_name, mimetype='xlsx')
+
+
 
 @app.route("/rates", methods=["POST"])
 def rates_create():
-  return "Not implemented"
+
+  try:      
+    dataframe = openpyxl.load_workbook("in/rates.xlsx")
+    dataframe1 = dataframe.active
     
+    testArray = []
+    # Iterate the loop to read the cell values
+    for row in range(1, dataframe1.max_row):
+      
+      testDict={}
+      for col in dataframe1.iter_cols(1, dataframe1.max_column):
+        
+        if col[0].value == "Scope":
+          testDict[col[0].value] = str(col[row].value)
+        elif col[0].value == "Product":
+          testDict[col[0].value] = str(col[row].value)    
+        else:
+          testDict[col[0].value] = int( col[row].value )
+
+      testArray.append(testDict)  
+    
+    for x in testArray:
+
+      if db.get_one_rate(x["Product"], x["Scope"]) != []:
+        print(x, "found one ===> ")
+        db.update_rates_same_pid_scope(x["Product"], x["Rate"], x["Scope"])
+      else:
+        db.create_rates(x["Product"], x["Rate"], x["Scope"])
+
+    return jsonify({"status_code": 200, "success": "true"})
+
+  except:
+    return jsonify({"status_code": 400, "success": "false"})    
+  
+    
+
+
 @app.route("/truck", methods=["POST"])
 def truck_create():
   number_plate = request.json.get('number_plate')
@@ -79,4 +132,4 @@ def health():
   return make_response("Failure", 500)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0', port=5000)
