abfeb57b40e223c1c1003b6a22c8127cef023e13
RJ-0605 <rojedkay@gmail.com>
2022-11-10 10:10:21 +0000

    remove post rates file upload

--- Billing/Billing.py
+++ Billing/Billing.py
@@ -47,26 +47,6 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/post_rates', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
-
-
-
 @app.route("/rates", methods=["POST"])
 def rates_create():
 
